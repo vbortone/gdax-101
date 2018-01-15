012cbe9e16e0c3fcc5cac0db5db418a1e064ba2d
Vincent Bortone <vbortone@gmail.com>
2018-01-15 02:17:52 -0500

    Add cli project

---
+++ src/gdax-cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gdax_cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
